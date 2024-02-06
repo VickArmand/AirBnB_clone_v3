9e78211ee7416dbde7178be752e3b6243419ab37
VickArmand <victormaina1962@gmail.com>
2024-02-06 04:43:36 +0300

    Status of your AP

---
+++ api/v1/app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+register the blueprint app_views to your Flask instance app
+"""
+from flask import Flask, render_template
+from models import storage
+from api.v1.views import app_views
+import os
+app = Flask(__name__)
+app.register_blueprint(app_views, url_prefix='/api/v1')
+
+
+@app.teardown_appcontext
+def teardown():
+    """
+    declare a method to handle @app.teardown_appcontext
+    that calls storage.close()
+    """
+    storage.close()
+
+
+if __name__ == '__main__':
+    h = os.getenv('HBNB_API_HOST') if os.getenv('HBNB_API_HOST') else '0.0.0.0'
+    p = os.getenv('HBNB_API_PORT') if os.getenv('HBNB_API_PORT') else 5000
+    app.run(host=h, port=p, threaded=True)
---
+++ api/v1/views/index.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""creates routes using blueprints"""
+from api.v1.views import app_views
+import json
+
+
+@app_views.route('/status')
+def ok():
+    """
+    create a route /status on the object app_views that
+    returns a JSON: "status": "OK
+    """
+    return json.dumps({'status': 'OK'})
