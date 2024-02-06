9f21c228f55fcef436d8a868ac105360ab46f30f
VickArmand <victormaina1962@gmail.com>
2024-02-06 05:41:22 +0300

    Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,8 +2,17 @@
 """creates routes using blueprints"""
 from api.v1.views import app_views
 import json
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
+classes = {"Amenity": Amenity, "City": City,
+           "Place": Place, "Review": Review, "State": State, "User": User}
 @app_views.route('/status')
 def ok():
     """
@@ -13,3 +22,14 @@ def ok():
     return json.dumps({
         'status': 'OK'
         })
+
+
+@app_views.route('/stats')
+def count():
+    keys = ["amenities", "cities", "places", "reviews", "states", "users"]
+    val = [Amenity, City, Place, Review, State, User]
+    resp = {}
+    for index in range(len(val)):
+        resp[keys[index]] = storage.count(val[index])
+    return json.dumps(resp)
+
