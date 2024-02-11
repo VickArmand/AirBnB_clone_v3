a06a2b0a46a55880ce17d177a38d635813fb489b
VickArmand <victormaina1962@gmail.com>
2024-02-11 10:07:46 +0300

    corrections v1

--- api/v1/app.py
+++ api/v1/app.py
@@ -6,7 +6,9 @@ to your Flask instance app
 import os
 from flask import Flask, render_template, jsonify
 from models import storage
+from api.v1.views import app_views
 app = Flask(__name__)
+app.register_blueprint(app_views, url_prefix='/api/v1')
 
 
 @app.teardown_appcontext
