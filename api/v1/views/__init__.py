f36dd9f09b7a1b4a68d3a51dce9225237daec55b
VickArmand <victormaina1962@gmail.com>
2024-02-11 10:00:40 +0300

    corrections v1

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -4,12 +4,8 @@ create a variable app_views which is an instance of Blueprint
 (url prefix must be /api/v1)
 """
 from flask import Blueprint
-app_views = Blueprint(
-        'app_views',
-        __name__,
-        static_folder='static',
-        template_folder='templates'
-        )
+app_views = Blueprint('app_views', __name__)
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
