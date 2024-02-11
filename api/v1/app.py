dd9c06c69203549477be1e6c38a2ccf6c6908cab
VickArmand <victormaina1962@gmail.com>
2024-02-11 11:03:43 +0300

    corrections v2 CORS

--- api/v1/app.py
+++ api/v1/app.py
@@ -10,7 +10,6 @@ from flask_cors import CORS
 from api.v1.views import app_views
 app = Flask(__name__)
 app.register_blueprint(app_views, url_prefix='/api/v1')
-CORS(app, resources={"/*": {"origins": "0.0.0.0"}})
 
 
 @app.teardown_appcontext
